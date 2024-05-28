b57820cd3f9e722d64e872770ec2b9e9e606a500
Nebot774 <adriclemenbot@gmail.com>
2024-05-28 16:12:13 +0200

    tot rulan,ara apunts

--- app/src/main/java/com/example/navigation_camino_del_guerrero/TabbedActivity.java
+++ app/src/main/java/com/example/navigation_camino_del_guerrero/TabbedActivity.java
@@ -1,4 +1,15 @@
 package com.example.navigation_camino_del_guerrero;
 
-public class TabbedActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class TabbedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabbed);
+
+    }
 }
